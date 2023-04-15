5fd7bb885ed7db5bd07fce4378999dfd43154cd2
Grabe-me <grabe85me@gmail.com>
2023-04-15 16:18:07 +0300

    postgre insert added

--- Postgresql.py
+++ Postgresql.py
@@ -18,3 +18,24 @@ book = Table('books', metadata,
 
 
 metadata.create_all(engine)
+insert_author = author.insert().values(
+    name='Michael',
+    surname='Bulgakov'
+)
+
+connection = engine.connect()
+author_ins = connection.execute(insert_author)
+connection.commit()
+
+
+insert_book = book.insert().values(
+    title='Master and Margaret',
+    author_id=author_ins.inserted_primary_key[0]
+)
+
+book_ins = connection.execute(insert_book)
+connection.commit()
+
+s = author.select()
+data = connection.execute(s)
+print(data.fetchall())
